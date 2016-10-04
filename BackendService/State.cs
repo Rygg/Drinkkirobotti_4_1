6ef82e1dc23f25cac13960134271d8bc7f126ed5
Oskari Burri <oskari.burri@student.tut.fi>
2016-10-04 16:28:51 +0300

    Added a structure for a static State object

---
+++ BackendService/State.cs
@@ -0,0 +1,19 @@
+﻿
+namespace SystemState
+{
+    public enum State
+    {
+        Startup = 0,
+        Idle = 1,
+        GrabBottle = 2,
+        PourBottle = 3,
+        ReturnBottle = 4,
+        RemoveBottle = 5,
+        GetNewBottle = 6,
+        Paused = 7,
+        PauseGetNewBottle = 8,
+        PauseGrabBottle = 9,
+        PauseRemoveBottle = 10,
+        PauseReturnBottle = 11,
+    }
+}
---
+++ BackendService/SystemState.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemState
+{
+    /// <summary>
+    /// Static class to store the current state of the system
+    /// </summary>
+    public static class SystemState
+    {
+        /// <summary>
+        /// Is the system in an error state
+        /// </summary>
+        private static bool ERROR = false;
+
+        /// <summary>
+        /// In which state the system is in
+        /// </summary>
+        private static State STATE = State.Startup;
+
+        /// <summary>
+        /// Get the systems state
+        /// </summary>
+        /// <returns>System current state</returns>
+        public static State GetState()
+        {
+            return STATE;
+        }
+
+        /// <summary>
+        /// Check if system is in error state
+        /// </summary>
+        /// <returns>Is the system in an error state</returns>
+        public static bool GetError()
+        {
+            return ERROR;
+        }
+    }
+}
